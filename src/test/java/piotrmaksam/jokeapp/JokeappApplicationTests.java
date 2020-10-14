eea5fda041818cfb2a0bd3b4ca2763f942183653
Piotr Maksam <pmaksam@gmail.com>
2020-10-14 16:02:16 +0200

    Initial commit

---
+++ src/test/java/piotrmaksam/jokeapp/JokeappApplicationTests.java
@@ -0,0 +1,13 @@
+package piotrmaksam.jokeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JokeappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
